27691da31261903cc55b6afe68e740d7598b7013
Vinali Samarasinghe <it21321368@my.sliit.lk>
2025-08-06 11:42:08 +0530

    create NavigationCommandTest

---
+++ src/test/java/NavigationCommandTest.java
@@ -0,0 +1,31 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class NavigationCommandTest {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = WebDriverManager.chromedriver().create();
+
+        driver.manage().window().maximize();
+
+        String url = "https://www.ebay.com/";
+        driver.get(url);
+
+        System.out.println("1 - " +driver.getTitle());
+
+        driver.findElement(By.xpath("//li/a[text()='Sports']")).click();
+        Thread.sleep(2000);
+        System.out.println("2 - " +driver.getTitle());
+
+        driver.navigate().back();
+        Thread.sleep(2000);
+        System.out.println("3 - " +driver.getTitle());
+
+        driver.navigate().forward();
+        Thread.sleep(2000);
+        System.out.println("4 - " +driver.getTitle());
+
+        driver.navigate().refresh();
+        driver.quit();
+    }
+}
