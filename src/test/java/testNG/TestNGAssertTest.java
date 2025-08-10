7e428cffdd6e2e298d5f4169e26d80a53ee17eee
Vinali Samarasinghe <it21321368@my.sliit.lk>
2025-08-10 23:11:21 +0530

    add TestNGAssertTest

--- src/test/java/testNG/TestNGAssertTest.java
+++ src/test/java/testNG/TestNGAssertTest.java
@@ -2,6 +2,7 @@ package testNG;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class TestNGAssertTest {
     @Test
@@ -13,5 +14,18 @@ public class TestNGAssertTest {
         Assert.assertEquals(name,"Aamal");
         System.out.println("C");
         System.out.println("D");
+    }
+
+    @Test
+    public void softAssertion(){
+        SoftAssert softAssert = new SoftAssert();
+
+        String name = "Kamal";
+        System.out.println("A");
+        System.out.println("B");
+        softAssert.assertEquals(name,"Amal");
+        System.out.println("C");
+        System.out.println("D");
+        softAssert.assertAll();
     }
 }
