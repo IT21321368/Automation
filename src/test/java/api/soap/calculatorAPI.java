e2388306d08ff8e69e365f0d6e33d6de86708b95
Vinali Samarasinghe <it21321368@my.sliit.lk>
2025-08-13 12:32:21 +0530

    add calculatorAPI

---
+++ src/test/java/api/soap/calculatorAPI.java
@@ -0,0 +1,27 @@
+package api.soap;
+
+import org.apache.commons.compress.utils.IOUtils;
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import static io.restassured.RestAssured.given;
+
+public class calculatorAPI {
+    @Test
+    public void add() throws FileNotFoundException {
+        InputStream inputStream = new FileInputStream(getClass().getClassLoader().getResource("add.xml").getFile());
+        String xmlBody = IOUtils.toString(inputStream);
+        given()
+                .contentType("text/xml")
+                .body(xmlBody)
+                .when()
+                .post("http://www.dneonline.com/calculator.asmx")
+                .then()
+                .log().all();
+
+
+    }
+}
